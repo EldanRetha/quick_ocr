b0008c25e4459453265def88f3077ca0aa16f493
Eldan Retha <me@eldan.io>
2023-10-30 17:29:08 -0400

    Added white background to be more like the snipping tool.
    Cleaned up code in various places.

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,10 @@ use softbuffer::{Context, Surface, Buffer};
 use std::collections::{HashMap};
 use std::num::NonZeroU32;
 use std::rc::Rc;
-use winit::window::WindowId;
 
 use imageproc::drawing::draw_hollow_rect_mut;
 use imageproc::rect::Rect;
-use image::{Rgba, EncodableLayout};
+use image::{Rgba, DynamicImage, GenericImageView, ImageBuffer, Pixel, GenericImage};
 
 
 fn main() -> Result<(), winit::error::EventLoopError>{
@@ -32,7 +31,7 @@ fn main() -> Result<(), winit::error::EventLoopError>{
 
     // Capture screens first so we don't obscure them with new windows
     let screens = Screen::all().unwrap();
-    let mut screen_caps: Vec<RgbaImage> = Vec::new();
+    let mut screen_caps = Vec::new();
     for screen in screens {
         screen_caps.push(screen.capture().unwrap());
     }
@@ -43,7 +42,7 @@ fn main() -> Result<(), winit::error::EventLoopError>{
     let mut monitor_index = 0;
     //let mut windows = Vec::new();
     for monitor in event_loop.available_monitors() {
-        let window = create_fake_desktop(&event_loop, &monitor, monitor_index, &screen_caps[monitor_index]);
+        let window = create_fake_desktop(&event_loop, &monitor, &screen_caps[monitor_index]);
         let window_ref = window.clone();
         // Transfer ownership of the window to windows so that window outlives the for loop 
         windows.insert(window.id(), window);
@@ -55,7 +54,6 @@ fn main() -> Result<(), winit::error::EventLoopError>{
     let mut mouse_down = false;
     let mut mouse_down_pos = PhysicalPosition { x: 0f64, y: 0f64 };
     let mut mouse_up_pos = PhysicalPosition { x: 0f64, y: 0f64 };
-    let mut current_window = WindowId::from(0);
     let result = event_loop.run(move |event, elwt| {
         if let Event::WindowEvent { event, window_id } = event {
             match event {
@@ -83,7 +81,6 @@ fn main() -> Result<(), winit::error::EventLoopError>{
                     (MouseButton::Left, ElementState::Pressed) => {
                         mouse_down_pos = mouse_pos;
                         mouse_down = true;
-                        current_window = window_id;
                     },
                     (MouseButton::Left, ElementState::Released) => {
                         mouse_up_pos = mouse_pos;
@@ -96,8 +93,8 @@ fn main() -> Result<(), winit::error::EventLoopError>{
                     _ => ()
                 },
                 WindowEvent::CursorMoved {
-                    device_id,
-                    position
+                    position,
+                    ..
                 } => match position {
                     position => {
                         mouse_pos = position;
@@ -131,29 +128,30 @@ fn calculate_rect(pos1: PhysicalPosition<f64>, pos2: PhysicalPosition<f64>) -> R
         .of_size(width as u32, height as u32)
 }
 
-fn create_fake_desktop(event_loop: &EventLoop<()>, monitor: &MonitorHandle, monitor_index: usize, image: &RgbaImage) -> Rc<Window> {
+fn create_fake_desktop(event_loop: &EventLoop<()>, monitor: &MonitorHandle, image: &RgbaImage) -> Rc<Window> {
     let window =Rc::new(WindowBuilder::new()
         .with_title("")
         .with_visible(false)
         .build(&event_loop)
         .unwrap());
+    let fullscreen = Some(Fullscreen::Borderless(Some(monitor.clone())));
+    window.set_fullscreen(fullscreen);
     window.set_skip_taskbar(true);
     window.set_cursor_icon(CursorIcon::Crosshair);
     
     let context = Context::new(window.clone()).unwrap();
     let mut surface = Surface::new(&context, window.clone()).unwrap();
 
-    surface.resize(NonZeroU32::new(image.width()).unwrap(), NonZeroU32::new(image.height()).unwrap());
+    surface.resize(NonZeroU32::new(image.width()).unwrap(), NonZeroU32::new(image.height()).unwrap()).expect("Failed to resize window");
 
     let mut buffer = surface.buffer_mut().unwrap();
     
-    buffer_image(&mut buffer, image);
+    draw_shaded_image(&mut buffer, image);
 
-    buffer.present();
-
-    let fullscreen = Some(Fullscreen::Borderless(Some(monitor.clone())));
-    window.set_fullscreen(fullscreen);
+    // Presenting the buffering after appears to cause an issue where the window isn't actually made visible
+    // immediately
     window.set_visible(true);
+    buffer.present().expect("Failed to present buffer");
 
     return window;
 }
@@ -162,42 +160,98 @@ fn on_mouse_drag(window: &Rc<Window>, image: &RgbaImage, rect: Rect) {
     let context = Context::new(window.clone()).unwrap();
     let mut surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
 
-    surface.resize(NonZeroU32::new(image.width()).unwrap(), NonZeroU32::new(image.height()).unwrap());
+    surface.resize(NonZeroU32::new(image.width()).unwrap(), NonZeroU32::new(image.height()).unwrap()).expect("Failed to resize window");
 
     let mut buffer = surface.buffer_mut().unwrap();
 
-    let image_with_square = draw_square_on_image(image, rect);
-    buffer_image(&mut buffer, &image_with_square);
-    buffer.present();
+    
+
+
+    let mut new_buffer = BufferWrapper{
+        buffer: &mut buffer,
+        width: image.width(),
+        height: image.height(),
+        x: rect.left() as u32,
+        y: rect.top() as u32
+    };
+    draw_shaded_image(&mut new_buffer.buffer, image);
+    let sub_image = image.view(rect.left() as u32, rect.top() as u32, rect.width(), rect.height());
+    new_buffer.copy_from(&*sub_image, rect.left() as u32, rect.top() as u32).expect("Failed to copy subimage");
+    draw_hollow_rect_mut(&mut new_buffer, rect, Rgba([255,0,0,255]));
+
+    buffer.present().expect("Failed to present buffer");
 }
 
 
-fn draw_square_on_image(image: &RgbaImage, rect: Rect) -> RgbaImage {
 
-    let mut out_image = image.clone();
+struct BufferWrapper<'a, 'b> {
+    buffer: &'a mut Buffer<'b, Rc<Window>,Rc<Window>>,
+    width: u32,
+    height: u32,
+    x: u32,
+    y: u32
+}
 
-    draw_hollow_rect_mut(&mut out_image, rect, Rgba([255,0,0,0]));
-    return out_image;
+impl<'a, 'b> GenericImageView for BufferWrapper<'a, 'b> {
+    type Pixel = image::Rgba<u8>;
+
+    fn dimensions(&self) -> (u32, u32) {
+        (self.width, self.height)
+    }
+    fn bounds(&self) -> (u32, u32, u32, u32) {
+        (self.x, self.y, self.width, self.height)
+    }
+
+    fn get_pixel(&self, x: u32, y: u32) -> Self::Pixel {
+        let color_u32 = self.buffer[(y * self.width + x) as usize];
+        let alpha = (color_u32 & 0xFF000000 >> 24) as u8;
+        let red = (color_u32 & 0x00FF0000 >> 16) as u8;
+        let green = (color_u32 & 0x0000FF00 >> 8) as u8;
+        let blue = (color_u32 & 0x000000FF) as u8;
+        Rgba::from([red, green, blue, alpha])
+    }
+
+}
+
+impl<'a, 'b> GenericImage for BufferWrapper<'a, 'b> {
+    fn put_pixel(&mut self, x: u32, y: u32, pixel: Self::Pixel) {
+        let red = pixel[0]  as u32;
+        let green = pixel[1] as u32;
+        let blue = pixel[2] as u32;
+        let alpha = pixel[3] as u32;
+        let color_u32 = blue | (green << 8) | (red << 16) | (alpha << 24);
+        self.buffer[(y * self.width + x) as usize] = color_u32;
+    }
+
+    fn get_pixel_mut(&mut self, x: u32, y: u32) -> &mut Self::Pixel {
+        todo!();
+    }
+
+    fn blend_pixel(&mut self, x: u32, y: u32, pixel: Self::Pixel) {
+        todo!();
+    }
 }
 
 
-fn buffer_image(buffer: &mut Buffer<'_, Rc<Window>, Rc<Window>>, image: &RgbaImage) {
-    let mut i = 0;
 
-    for pixel in image.pixels() {
-        let red = pixel.0[0] as u32;
-        let green = pixel.0[1] as u32;
-        let blue = pixel.0[2] as u32;
+fn draw_shaded_image(buffer: &mut Buffer<'_, Rc<Window>, Rc<Window>>, image: &RgbaImage) {
+    for (i, pixel) in image.pixels().enumerate() {
+        let red;
+        let green;
+        let blue;
+        let u8max = u8::MAX as u32;
+        
+        red = (pixel.0[0] as u32 + 50).min(u8max) ;
+        green = (pixel.0[1] as u32 + 50).min(u8max);
+        blue = (pixel.0[2] as u32 + 50).min(u8max);
 
         let color = blue | (green << 8) | (red << 16) ;
         buffer[i] = color;
-        i = i+1;
     }
 }
 
 fn process_image(image: &RgbaImage, rect: Rect) {
     use rusty_tesseract::{Image, Args};
-    use image::DynamicImage;
     use clipboard_win::{Clipboard, formats, Setter};
     
     // TODO: Add bounds checks
